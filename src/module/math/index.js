47ee16dd044b8e65b730799790f86681a65845ba
wzn <997402405@qq.com>
2018-01-31 16:07:34 +0800

    测试

---
+++ src/module/math/index.js
@@ -0,0 +1,119 @@
+let math = {
+ /**
+ * 
+ * 两个数相加
+ * @param {any} arg1 
+ * @param {any} arg2 
+ * @returns 
+ */
+add : (arg1, arg2) => {
+        arg1 = arg1 || 0;
+        arg2 = arg2 || 0;
+        var r1, r2, m, c;
+        try {
+            r1 = arg1.toString().split(".")[1].length;
+        } catch (e) {
+            r1 = 0;
+        }
+        try {
+            r2 = arg2.toString().split(".")[1].length;
+        } catch (e) {
+            r2 = 0;
+        }
+        c = Math.abs(r1 - r2);
+        m = Math.pow(10, Math.max(r1, r2));
+        if (c > 0) {
+            var cm = Math.pow(10, c);
+            if (r1 > r2) {
+                arg1 = Number(arg1.toString().replace(".", ""));
+                arg2 = Number(arg2.toString().replace(".", "")) * cm;
+            } else {
+                arg1 = Number(arg1.toString().replace(".", "")) * cm;
+                arg2 = Number(arg2.toString().replace(".", ""));
+            }
+        } else {
+            arg1 = Number(arg1.toString().replace(".", ""));
+            arg2 = Number(arg2.toString().replace(".", ""));
+        }
+        return (arg1 + arg2) / m;
+    },
+    /**
+     * 
+     * 两个数想减
+     * @param {any} arg1 
+     * @param {any} arg2 
+     * @returns 
+     */
+    sub : (arg1, arg2) => {
+
+        arg1 = arg1 || 0;
+        arg2 = arg2 || 0;
+        var r1, r2, m, n;
+        try {
+            r1 = arg1.toString().split(".")[1].length;
+        } catch (e) {
+            r1 = 0;
+        }
+        try {
+            r2 = arg2.toString().split(".")[1].length;
+        } catch (e) {
+            r2 = 0;
+        }
+        m = Math.pow(10, Math.max(r1, r2));
+        n = r1 >= r2 ? r1 : r2;
+        return ((arg1 * m - arg2 * m) / m).toFixed(n);
+    },
+    /**
+     * 
+     * 两个数相乘
+     * @param {any} arg1 
+     * @param {any} arg2 
+     * @returns 
+     */
+    mul : (arg1, arg2) => {
+        arg1 = arg1 || 0;
+        arg2 = arg2 || 0;
+        var m = 0,
+            s1 = arg1.toString(),
+            s2 = arg2.toString();
+        try {
+            m += s1.split(".")[1].length;
+        } catch (e) {}
+        try {
+            m += s2.split(".")[1].length;
+        } catch (e) {}
+        return (
+            Number(s1.replace(".", "")) *
+            Number(s2.replace(".", "")) /
+            Math.pow(10, m)
+        );
+    },
+     /**
+     * 
+     * 两个数相除
+     * @param {any} arg1 
+     * @param {any} arg2 
+     * @returns 
+     */
+    div : (arg1, arg2) => {
+        arg1 = arg1 || 0;
+        arg2 = arg2 || 0;
+        var t1 = 0,
+            t2 = 0,
+            r1,
+            r2;
+        try {
+            t1 = arg1.toString().split(".")[1].length;
+        } catch (e) {}
+        try {
+            t2 = arg2.toString().split(".")[1].length;
+        } catch (e) {}
+        r1 = Number(arg1.toString().replace(".", ""));
+        r2 = Number(arg2.toString().replace(".", ""));
+        return r1 / r2 * Math.pow(10, t2 - t1);
+    },
+    reduce: (args,fn) => {
+      return Array.prototype.slice.call(args).reduce
+    }    
+}
+export default math;
