25556dc5370bd6c33b18e038253d3d9478de7c01
wzn <997402405@qq.com>
2018-02-06 09:38:42 +0800

    手机脱敏

--- test/other.test.js
+++ test/other.test.js
@@ -2,18 +2,18 @@ import other from "../src/module/other";
 import chai from "chai";
 let expect = chai.expect;
 
-describe("输出金额格式化以后的数据", function() {
-    it("应该输出金额格式化以后的数据", function() {
-      expect(
-        other.formatMoney(19000)
-      ).to.be.equal('19,000.00');
-    });
+describe("输出金额格式化以后的数据", function () {
+  it("应该输出金额格式化以后的数据", function () {
+    expect(
+      other.formatMoney(19000)
+    ).to.be.equal('19,000.00');
   });
+});
 
-  describe("输出脱敏以后的手机号码", function() {
-    it("应该输出脱敏以后的手机号码", function() {
-      expect(
-        other.formatPhone('13368343974')
-      ).to.be.equal('133****3974');
-    });
+describe("输出脱敏以后的手机号码", function () {
+  it("应该输出脱敏以后的手机号码", function () {
+    expect(
+      other.formatPhone('13368343974')
+    ).to.be.equal('133****3974');
   });
+});
