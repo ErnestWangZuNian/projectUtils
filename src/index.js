e8e41e00b43433b36d99335a0e39be1761d35d6d
wzn <997402405@qq.com>
2019-11-06 17:11:37 +0800

    更新类型判断

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import array from './module/array'
-import date from './module/date'
-import obj from './module/object'
-import math from './module/math'
-import regular from './module/regular'
-import other from './module/other'
-import storage from './module/storage'
+import array from './module/array';
+import date from './module/date';
+import obj from './module/object';
+import math from './module/math';
+import regular from './module/regular';
+import other from './module/other';
+import storage from './module/storage';
 
 let utils = {
-    array,
-    date,
-    obj,
-    math,
-    regular,
-    other,
-    storage
-}
+  array,
+  date,
+  obj,
+  math,
+  regular,
+  other,
+  storage,
+  isArray: (target) => Object.prototype.toString.call(target) === '[object Array]',
+  isFunction: (target) => Object.prototype.toString.call(target) === '[object Function]',
+  isBoolean: (target) => Object.prototype.toString.call(target) === '[object Boolean]',
+  isObject: (target) => Object.prototype.toString.call(target) === '[object Object]',
+  isEmoptyObject: (target) => {
+    let result = false;
+    if (!Object.keys(target).length) {
+      result = true;
+    }
+    return result;
+  },
+  isNumber: (target) => Object.prototype.toString.call(target) === '[object Number]',
+  isString: (target) => Object.prototype.toString.call(target) === '[object String]',
+};
 
 export default utils;
